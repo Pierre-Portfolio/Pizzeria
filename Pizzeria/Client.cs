08251323dbf7819973933d77fcbf5dc6be23bebb
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2020-12-30 15:59:00 +0100

    create client.cs

--- Pizzeria/Client.cs
+++ Pizzeria/Client.cs
@@ -6,5 +6,23 @@ namespace Pizzeria
 {
     class Client
     {
+        private bool FirstCommande;
+        private int NumClient;
+        private string AdrClient;
+        private string NomClient;
+        private string PrenomClient;
+        private DateTime DatePremiereCmd;
+        private int CumulCommande;
+
+        public Client(bool FirstCommande, int NumClient, string AdrClient, string NomClient, string PrenomClient, DateTime DatePremiereCmd, int CumulCommande)
+        {
+            this.FirstCommande = FirstCommande;
+            this.NumClient = NumClient;
+            this.AdrClient = AdrClient;
+            this.NomClient = NomClient;
+            this.PrenomClient = PrenomClient;
+            this.DatePremiereCmd = DatePremiereCmd;
+            this.CumulCommande = CumulCommande;
+        }
     }
 }
