751a3cec4688d53a60c8e3258328b9576efd2f8a
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2020-12-30 10:04:26 +0100

    force commit

--- Pizzeria/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Pizzeria/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Pizzeria {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Pizzeria;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Pizzeria;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
