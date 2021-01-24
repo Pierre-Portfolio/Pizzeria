3665116cf4ab0fa5b8ab4de3d3d48a383144feaf
Flaye <tom.payet97419@gmail.com>
2021-01-24 11:20:59 +0100

    Creation page AddClient.xaml
    La page est créée sans possibiliré de l'ouvrir

---
+++ Pizzeria/AddClient.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pizzeria
+{
+    /// <summary>
+    /// Logique d'interaction pour AddClient.xaml
+    /// </summary>
+    public partial class AddClient : Window
+    {
+        public AddClient()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
