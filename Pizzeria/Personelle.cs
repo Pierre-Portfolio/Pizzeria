b8e4f86a068eb4f27865d02f3d892ab6b74d8319
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-01-29 16:40:42 +0100

    Update Personelle.cs

--- Pizzeria/Personelle.cs
+++ Pizzeria/Personelle.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace Pizzeria
 {
-    public abstract class Personelle
+    public abstract class Personelle : INotifyPropertyChanged
     {
         protected string nomEmploye;
         protected string prenomEmploye;
         protected string adrEmploye;
         protected string mdpEmploye;
         protected string numEmploye;
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public Personelle(string nomEmploye, string prenomEmploye, string adrEmploye, string mdpEmploye, string numEmploye = null)
         {
@@ -23,27 +25,38 @@ namespace Pizzeria
         public string NomEmploye
         {
             get { return nomEmploye; }
+            set { this.nomEmploye = value; OnPropertyChanged("NomEmploye"); }
         }
 
         public string PrenomEmploye
         {
             get { return prenomEmploye; }
+            set { this.prenomEmploye = value; OnPropertyChanged("PrenomEmploye"); }
         }
 
         public string AdrEmploye
         {
             get { return adrEmploye; }
-            set { this.adrEmploye = value; }
+            set { this.adrEmploye = value; OnPropertyChanged("AdrEmploye"); }
         }
 
         public string MdpEmploye
         {
             get { return this.mdpEmploye; }
+            set { this.mdpEmploye = value; OnPropertyChanged("MdpEmploye"); }
         }
         public string NumEmploye
         {
             get { return numEmploye; }
-            set { this.numEmploye = value; }
+            set { this.numEmploye = value; OnPropertyChanged("NumEmploye"); }
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
